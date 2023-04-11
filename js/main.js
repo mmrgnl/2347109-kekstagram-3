adb094de720b205f31c8db5e33d77cac2abc3c46
Maksim Pisarik <m.p.max@live.com>
2023-04-12 00:43:26 +0300

    Module 5

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
-function getRandom(min, max) {   // Результат: целое число из диапазона "от...до"
-  if (min < 0) {
-    min = Math.abs(max);
-  }
-  if (max < 0) {
-    max = Math.abs(max);
-  }
-
-  if (min > max) {
-    const i = max;
-    max = min;
-    min = i;
-  }
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function checkMaxLength(string, maxLength) {
-  return string.length <= maxLength;
-}
-
-
-getRandom(15, 25);
+import { generatePhotoData } from './data.js';
+import { checkMaxLength } from 'util.js';
 
 checkMaxLength('Hello world!', 255);
-
-
+generatePhotoData(20);
