c99a2540adff27136a0213ca8f0b184ba42e3f0e
Maksim Pisarik <m.p.max@live.com>
2023-05-10 17:27:40 +0300

    Module 4-2

--- js/main.js
+++ js/main.js
@@ -20,6 +20,22 @@ function checkMaxLength(string, maxLength) {
   return string.length <= maxLength;
 }
 
+function generatePhotos(count) {
+  const generateSingle = (i) => ({
+    id: i,
+    url: `photos/${i + 1}.jpg`,
+    description: 'Here is some random string: \'39CDF217\'',
+    likes: getRandom(15, 200),
+    comments: getRandom(0, 200),
+  });
+
+  const photoData = [];
+  for (let i = 0; i < count; i++) {
+    photoData.push(generateSingle(i));
+  }
+  return photoData;
+}
+
 
 getRandom(15, 25);
 
