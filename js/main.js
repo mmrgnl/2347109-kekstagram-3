d3abb0eb4cdc4218c9e278914929bdad99e45dff
Maksim Pisarik <m.p.max@live.com>
2023-05-14 00:37:12 +0300

    Module 8-2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { generatePhotos} from './data.js';
 import { renderingPhotoMiniature } from './rendering.js';
+import './form.js';
+import './formValidator.js';
 
 const photos = generatePhotos(25);
 renderingPhotoMiniature(photos);
-
-//Задание 7 часть 2
